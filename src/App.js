761d7216c55e1641542561842e3db55fd8fcaa46
Seifeddine Elkorichi <seifeddine.elkorichi@hotmil.com>
2021-05-22 21:20:59 +0100

    adding redux, store, reducer, actions, ...

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import React from "react";
+import { Provider } from "react-redux";
+import Counter from "./components/Counter";
+import store from "./components/Store";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Counter />
+    </Provider>
   );
 }
-
 export default App;
